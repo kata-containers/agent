c706c71b9d7ecd0d4edb7ef1408653ca2d9e9fa7
Peng Tao <bergwolf@gmail.com>
2017-12-11 18:21:13 +0800

    generate coverage report
    Signed-off-by: Peng Tao <bergwolf@gmail.com>

--- hack/go-test.sh
+++ hack/go-test.sh
@@ -7,6 +7,6 @@ set -e
 test_dirs=$(go list ./... | grep -v vendor)
 for testdir in ${test_dirs}; do
     echo start testing $testdir
-    go test -v $testdir
+    go test -v $testdir -coverprofile=coverage.txt -covermode=atomic
 done
 set +e
