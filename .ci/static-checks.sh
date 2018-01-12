9ed615c403424ecff5fd141029097d7d54dc78bf
James O. D. Hunt <james.o.hunt@intel.com>
2018-01-12 15:37:12 +0000

    CI: switch to central static check script
    Switch the CI script to use the central script in the tests repository.
    
    Fixes #90.
    
    Signed-off-by: James O. D. Hunt <james.o.hunt@intel.com>

--- .ci/static-checks.sh
+++ .ci/static-checks.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #
-# Copyright (c) 2018 Intel Corporation
+# Copyright (c) 2017-2018 Intel Corporation
 #
 # SPDX-License-Identifier: Apache-2.0
 
 set -e
 
 cidir=$(dirname "$0")
+source "${cidir}/lib.sh"
 
-bash -f "$cidir/static-checks.sh"
+run_static_checks
