df26e80df779ebd069613f831360d5a56a732111
Peng Tao <bergwolf@gmail.com>
2018-09-03 17:55:20 +0800

    device: do not close notify channel when wait timeout
    Otherwise we might race with the uevent watcher and close
    the same channel twice, causing agent to crash like:
    
    msg="panic: close of nil channel\n" name=kata-proxy pid=38265 sandbox=6c9e75c9-9815-4e85-91da-2cb1bdebb085 source=agent
    msg="\n" name=kata-proxy pid=38265 sandbox=6c9e75c9-9815-4e85-91da-2cb1bdebb085 source=agent
    msg="goroutine 33 [running]:\n" name=kata-proxy pid=38265 sandbox=6c9e75c9-9815-4e85-91da-2cb1bdebb085 source=agent
    msg="main.getPCIDeviceName(0xc4200711e0, 0xc420013dfa, 0x5, 0x1, 0xc4201df188, 0x0, 0xc420072480)\n" name=kata-proxy pid=38265 sandbox=6c9e75c9-9815-4e85-91da-2cb1bdebb085 source=agent
    msg="\t/golang/src/github.com/kata-containers/agent/device.go:143 +0x4f9\n" name=kata-proxy pid=38265 sandbox=6c9e75c9-9815-4e85-91da-2cb1bdebb085 source=agent
    
    Signed-off-by: Peng Tao <bergwolf@gmail.com>

--- device.go
+++ device.go
@@ -140,7 +140,6 @@ func getBlockDeviceNodeName(s *sandbox, pciID string) (string, error) {
 		case <-time.After(time.Duration(timeoutHotplug) * time.Second):
 			s.Lock()
 			delete(s.deviceWatchers, pciAddr)
-			close(notifyChan)
 			s.Unlock()
 
 			return "", grpcStatus.Errorf(codes.DeadlineExceeded,
