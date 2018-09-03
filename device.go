7caf1c8bd9ebad9ab6631e1839d62c8c98ed7d8a
Peng Tao <bergwolf@gmail.com>
2018-09-03 19:58:00 +0800

    device: check for existing device PCI path before waiting
    If agent is up AFTER device is hotplugged, it will miss the window
    of catching the device hotplug uevent and as a result it cannot fill
    it in the device pci map.
    
    We should check for such case before trying to wait otherwise the
    waiting will always fail timeout.
    
    Signed-off-by: Peng Tao <bergwolf@gmail.com>

--- device.go
+++ device.go
@@ -112,14 +112,23 @@ func getPCIDeviceName(s *sandbox, pciID string) (string, error) {
 
 	fieldLogger := agentLog.WithField("pciID", pciID)
 
-	// Check if the PCI identifier is in PCI device map.
 	s.Lock()
+	// Check if the PCI identifier is in PCI device map.
 	for key, value := range s.pciDeviceMap {
 		if strings.Contains(key, pciAddr) {
 			devName = value
 			fieldLogger.Info("Device found in pci device map")
 			break
 		}
+	}
+
+	// Check if the PCI path is present before we setup the pci device map.
+	_, err = os.Stat(filepath.Join(rootBusPath, pciAddr))
+	if err == nil {
+		// Found pci path. It's there before we wait for the device map.
+		// We cannot find the name for it.
+		fieldLogger.Info("Device found on pci device path")
+		return "", nil
 	}
 
 	// If device is not found in the device map, hotplug event has not
@@ -159,6 +168,9 @@ func virtioBlkDeviceHandler(device pb.Device, spec *pb.Spec, s *sandbox) error {
 	devPath, err := getPCIDeviceName(s, device.Id)
 	if err != nil {
 		return err
+	}
+	if devPath == "" {
+		return fmt.Errorf("cannot find device name for virtio block device")
 	}
 	device.VmPath = devPath
 
